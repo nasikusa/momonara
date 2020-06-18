5d3cd4b486bc43aee2fbc503a32ae0217bf2673d
youkan <youkan94@gmail.com>
2020-06-18 23:54:28 +0900

    fix: 未作成のページへのリンクを削除

--- resources/views/main_view/footer.blade.php
+++ resources/views/main_view/footer.blade.php
@@ -2,9 +2,9 @@
 
 	<div class="footer_wrapper_1">
 		<div><a href="/about">momonaraとは？</a></div>
-		<div><a href="/termsofuse">利用規約</a></div>
-		<div><a href="/privacy">プライバシー</a></div>
-		<div><a href="/question">お問い合わせ</a></div>
+		{{-- <div><a href="/termsofuse">利用規約</a></div> --}}
+		{{-- <div><a href="/privacy">プライバシー</a></div> --}}
+		{{-- <div><a href="/question">お問い合わせ</a></div> --}}
 
 		 @if (  \Auth::user() )
 
